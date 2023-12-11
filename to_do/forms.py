1b95a72e8e317d35653b32dc4301bff840cdd14e
eclimacoh <eclimacoh@pj.gob.pe>
2023-12-10 20:05:29 -0500

    to_do: Tabler.io modal con validación de errores

--- to_do/forms.py
+++ to_do/forms.py
@@ -4,7 +4,12 @@ from .models import Tasks
 
 
 class CreateTaskForm(forms.ModelForm):
-    # name = forms.CharField(label = "Tarea", widget = forms.TextInput( attrs={'class': 'form-control'} ))
+    # name = forms.CharField(
+    #     label = "Tarea",
+    #     error_messages=[
+    #         'unique':
+    #     ],
+    #     widget = forms.TextInput( attrs={'class': 'form-control'} ))
     # priority = forms.CharField(label = "Prioridad", widget = forms.Select(choices=(('L', 'BAJO'), ('M', 'MEDIO'), ('H', 'ALTO')), attrs={'class': 'form-select'} ))
     # description = forms.CharField(label = "Description", widget = forms.Textarea( attrs={'class': 'form-control', 'rows': '5'} ))
     # year = forms.CharField(label = "Year", widget = forms.NumberInput( attrs={'class': 'form-control'} ))
@@ -25,6 +30,9 @@ class CreateTaskForm(forms.ModelForm):
                 }
             ),
         }
+        error_messages = {
+            'unique': 'blaaaa'
+        }
 
     # def clean(self):
     #     super().clean()
@@ -41,8 +49,13 @@ class CreateTaskForm(forms.ModelForm):
     def clean_name(self):
         # super().clean()
         name = self.cleaned_data.get('name')
+
         if len(name) < 3:
             self.add_error('name','La tarea debe tener más de tres caracteres')
             self.fields['name'].widget.attrs.update({'class': 'form-control is-invalid'})
             print('Name must be more than 3 characters NAME')
+        if Tasks.objects.filter(name=name).exists():
+            self.add_error('name', f'Ya existe una tarea con el mismo nombre {name}.')
+            self.fields['name'].widget.attrs.update({'class': 'form-control is-invalid'})
+            print(f'Ya existe una tarea con el mismo nombre {name}.')
         return name
